2b6cdf1f87f5b4057ae804cbf28a6661814e3e3f
Anjali Krishna <anjalikrishna@anjalis-mbp.home>
2018-03-08 16:47:53 -0600

    commiting

--- config.py
+++
@@ -1,5 +0,0 @@
-# Twitter API Keys
-consumer_key = '8bAqZRDz1AAJIbe8VxKNBHoYe'
-consumer_secret = 'HwJwkC6bxWsciPmIvRlxfXylJtklemjWlxdfLk7aONjSgVOImX'
-access_token = '258311993-xS77BMv8jxFtGHFSUFCLGN0TXitNZmzrFz1dDars'
-access_token_secret = '7IyFNBVcxMcJKGaz1TyzjY8fORIqvt53a4uyYPgVJcwoT'
